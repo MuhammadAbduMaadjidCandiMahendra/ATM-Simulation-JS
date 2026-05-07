b7a3999b194ebb1b1ce8ad3676a5a68434fcaa49
MuhammadAbduMaadjidCandiMahendra <MuhammadAbduMaadjid.CandiMahendra@mitrais.com>
2026-05-08 00:10:53 +0700

    Add withdraw content

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 let contentTitle = document.getElementById("content-title");
 let dashboardContent = document.getElementById("dashboard-content");
 let depositContent = document.getElementById("deposit-content");
+let withdrawContent = document.getElementById("withdraw-content");
 
 const showDashboardContent = () => {
   dashboardContent.style.display = "block";
   depositContent.style.display = "none";
+  withdrawContent.style.display = "none";
   contentTitle.innerHTML = "Dashboard";
 }
 
 const showDepositContent = () => {
   dashboardContent.style.display = "none";
   depositContent.style.display = "block";
+  withdrawContent.style.display = "none";
   contentTitle.innerHTML = "Deposit";
+}
+
+const showWithdrawContent = () => {
+  dashboardContent.style.display = "none";
+  depositContent.style.display = "none";
+  withdrawContent.style.display = "block";
+  contentTitle.innerHTML = "Withdraw";
 }
 
 document.getElementById("to-deposit").addEventListener("click", (event) => {
@@ -22,4 +32,9 @@ document.getElementById("to-deposit").addEventListener("click", (event) => {
 document.getElementById("to-dashboard").addEventListener("click", (event) => {
   event.preventDefault();
   showDashboardContent();
+})
+
+document.getElementById("to-withdraw").addEventListener("click", (event) => {
+  event.preventDefault();
+  showWithdrawContent();
 })

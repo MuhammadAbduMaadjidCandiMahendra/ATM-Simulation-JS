e72f28ac5dfc0390d9dc5a23d630d6307fe59b44
MuhammadAbduMaadjidCandiMahendra <MuhammadAbduMaadjid.CandiMahendra@mitrais.com>
2026-05-07 23:41:09 +0700

    Initial project

---
+++ main.js
@@ -0,0 +1,28 @@
+document.getElementById("title")
+  .innerHTML = "ATM Simulation";
+
+let contentTitle = document.getElementById("content-title");
+let dashboardContent = document.getElementById("dashboard-content");
+let depositContent = document.getElementById("deposit-content");
+
+const showDashboardContent = () => {
+  dashboardContent.style.display = "block";
+  depositContent.style.display = "none";
+  contentTitle.innerHTML = "Dashboard";
+}
+
+const showDepositContent = () => {
+  dashboardContent.style.display = "none";
+  depositContent.style.display = "block";
+  contentTitle.innerHTML = "Deposit";
+}
+
+document.getElementById("to-deposit").addEventListener("click", (event) => {
+  event.preventDefault();
+  showDepositContent();
+})
+
+document.getElementById("to-dashboard").addEventListener("click", (event) => {
+  event.preventDefault();
+  showDashboardContent();
+})

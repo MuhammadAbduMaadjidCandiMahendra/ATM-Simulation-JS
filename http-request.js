0fbe521190c292a885ff91c31657fa114897c38a
MuhammadAbduMaadjidCandiMahendra <MuhammadAbduMaadjid.CandiMahendra@mitrais.com>
2026-05-12 13:19:10 +0700

    Feat(http request): Add withdraw function and create function docs

--- http-request.js
+++ http-request.js
@@ -1,12 +1,3 @@
-// BE error response example:
-// {
-//   "detail": "Account number cannot be empty",
-//   "instance": "/login",
-//   "status": 400,
-//   "title": "Bad Request",
-//   "type": "https://localhost:8080/problems/bad-request"
-// }
-
 export const login = async (accountNumber) => {
   let response = await fetch("http://localhost:8080/login", {
     method: "POST",
@@ -21,6 +12,31 @@ export const login = async (accountNumber) => {
   return await response.json();
 }
 
+/**
+ * Find account by account number.
+ * Following is an example of success response body
+ * <pre>
+ * {
+ *   "accountNumber": "100001",
+ *   "name": "John Doe",
+ *   "balance": 5000
+ * }
+ * </pre>
+ *
+ * Following is an example of error response body:
+ * <pre>
+ * {
+ *   "detail": "Account not found",
+ *   "instance": "/account/10000",
+ *   "status": 400,
+ *   "title": "Bad Request",
+ *   "type": "https://localhost:8080/problems/bad-request"
+ * }
+ * </pre>
+ *
+ * @param accountNumber
+ * @return {Promise<*>}
+ */
 export const findAccountByAccountNumber = async (accountNumber) => {
   let response = await fetch(`http://localhost:8080/account/${accountNumber}`, {
     method: "GET",
@@ -31,4 +47,41 @@ export const findAccountByAccountNumber = async (accountNumber) => {
   }
 
   return await response.json();
+}
+
+/**
+ * Withdraw money from an account.
+ * If successful, the server will return HTTP status 200 without any content.
+ *
+ * Any return status other than 200 will be treated as an error.
+ * Following is an example of an error response body:
+ * <pre>
+ * {
+ *   "detail": "Insufficient balance",
+ *   "instance": "/transaction/withdraw",
+ *   "status": 400,
+ *   "title": "Bad Request",
+ *   "type": "https://localhost:8080/problems/bad-request"
+ * }
+ * </pre>
+ *
+ * @param accountNumber
+ * @param amount
+ * @return {Promise<void>}
+ */
+export const withdraw = async (accountNumber, amount) => {
+  const response = await fetch(`http://localhost:8080/transaction/withdraw`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      accountNumber,
+      amount,
+    })
+  });
+
+  if (!response.ok) {
+    throw response;
+  }
 }

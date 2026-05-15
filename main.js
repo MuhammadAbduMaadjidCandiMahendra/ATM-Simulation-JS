449ce2511d710b24f13c475cd51485b1aa2c2993
MuhammadAbduMaadjidCandiMahendra <MuhammadAbduMaadjid.CandiMahendra@mitrais.com>
2026-05-15 08:48:42 +0700

    Fix(dasboard): Fix form initialization

--- main.js
+++ main.js
@@ -74,6 +74,7 @@ const replaceContent = (content) => {
   mainContent.append(content);
   contentTitle.innerHTML = "Dashboard";
 }
+
 linkToDashboard.addEventListener("click", async (event) => {
   event.preventDefault();
   const {content, form} = dashboardContent();
@@ -83,28 +84,28 @@ linkToDashboard.addEventListener("click", async (event) => {
   const accountNumber = globalThis.localStorage.getItem(ACCOUNT_NUMBER_LOCAL_STORAGE_KEY);
   if (accountNumber) {
     const account = await findAccountByAccountNumber(accountNumber);
-    formAccountInfo.elements["accountNumber"].value = account.accountNumber;
-    formAccountInfo.elements["accountName"].value = account.name;
-    formAccountInfo.elements["balance"].value = account.balance;
+    form.elements["accountNumber"].value = account.accountNumber;
+    form.elements["accountName"].value = account.name;
+    form.elements["balance"].value = account.balance;
   }
 
   form.addEventListener("submit", dashboardFormEventListener(form));
 });
 
-linkToWithdraw.addEventListener("click", async (event) => {
-  event.preventDefault();
-  showWithdrawContent();
-  activateNavItem(linkToWithdraw);
-  clearForm(formWithdraw);
-  hideSuccessInput(document.getElementById("withdrawSuccess"));
-
-  const accountNumber = globalThis.localStorage.getItem(ACCOUNT_NUMBER_LOCAL_STORAGE_KEY);
-  if (accountNumber) {
-    const account = await findAccountByAccountNumber(accountNumber);
-    formWithdraw.elements["accountNumber"].value = account.accountNumber;
-    formWithdraw.elements["balance"].value = account.balance;
-  }
-});
+// linkToWithdraw.addEventListener("click", async (event) => {
+//   event.preventDefault();
+//   showWithdrawContent();
+//   activateNavItem(linkToWithdraw);
+//   clearForm(formWithdraw);
+//   hideSuccessInput(document.getElementById("withdrawSuccess"));
+//
+//   const accountNumber = globalThis.localStorage.getItem(ACCOUNT_NUMBER_LOCAL_STORAGE_KEY);
+//   if (accountNumber) {
+//     const account = await findAccountByAccountNumber(accountNumber);
+//     formWithdraw.elements["accountNumber"].value = account.accountNumber;
+//     formWithdraw.elements["balance"].value = account.balance;
+//   }
+// });
 
 const showErrorInput = (element, errorMessage) => {
   element.classList.add("input-error");
@@ -123,37 +124,37 @@ const hideSuccessInput = (element) => {
   element.classList.remove("success-message");
 }
 
-formWithdraw.addEventListener("submit", async (event) => {
-  event.preventDefault();
-  clearForm(formWithdraw, "accountNumber", "balance", "withdrawAmount");
-  hideSuccessInput(document.getElementById("withdrawSuccess"));
-
-  const formData = new FormData(formWithdraw);
-  const accountNumber = formData.get("accountNumber");
-  const withdrawAmount = formData.get("withdrawAmount");
-
-  if (!accountNumber) {
-    showErrorInput(formWithdraw.elements["accountNumber"], "Account number is required");
-    return;
-  }
-
-  if (!withdrawAmount) {
-    showErrorInput(formWithdraw.elements["withdrawAmount"], "Withdraw amount is required");
-    return;
-  }
-
-  if (withdrawAmount <= 0) {
-    showErrorInput(formWithdraw.elements["withdrawAmount"], "Should be greater than 0");
-    return;
-  }
-
-  try {
-    const account = await withdraw(accountNumber, withdrawAmount);
-    formWithdraw.elements["balance"].value = account.balance;
-    showSuccessInput(document.getElementById("withdrawSuccess"), `Withdraw ${withdrawAmount} successful`);
-  } catch (error) {
-    const errBody = await error.json();
-    showErrorInput(formWithdraw.elements["withdrawAmount"], errBody.detail);
-  }
-});
+// formWithdraw.addEventListener("submit", async (event) => {
+//   event.preventDefault();
+//   clearForm(formWithdraw, "accountNumber", "balance", "withdrawAmount");
+//   hideSuccessInput(document.getElementById("withdrawSuccess"));
+//
+//   const formData = new FormData(formWithdraw);
+//   const accountNumber = formData.get("accountNumber");
+//   const withdrawAmount = formData.get("withdrawAmount");
+//
+//   if (!accountNumber) {
+//     showErrorInput(formWithdraw.elements["accountNumber"], "Account number is required");
+//     return;
+//   }
+//
+//   if (!withdrawAmount) {
+//     showErrorInput(formWithdraw.elements["withdrawAmount"], "Withdraw amount is required");
+//     return;
+//   }
+//
+//   if (withdrawAmount <= 0) {
+//     showErrorInput(formWithdraw.elements["withdrawAmount"], "Should be greater than 0");
+//     return;
+//   }
+//
+//   try {
+//     const account = await withdraw(accountNumber, withdrawAmount);
+//     formWithdraw.elements["balance"].value = account.balance;
+//     showSuccessInput(document.getElementById("withdrawSuccess"), `Withdraw ${withdrawAmount} successful`);
+//   } catch (error) {
+//     const errBody = await error.json();
+//     showErrorInput(formWithdraw.elements["withdrawAmount"], errBody.detail);
+//   }
+// });
 
